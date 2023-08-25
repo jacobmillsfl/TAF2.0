7425213ed950d36514707f8fe62a10c5cd55b4f6
= <=>
2023-08-24 18:28:15 -0600

    Updated serverside uwsgi settings

--- backend/init_script.sh
+++ backend/init_script.sh
@@ -2,4 +2,4 @@
 # source tafapi/bin/activate
 # pip install --upgrade pip
 # pip install --no-cache-dir --upgrade -r ./requirements.txt
-uwsgi --ini uwsgi.ini --socket 0.0.0.0:5000 --enable-threads --protocol=http -w wsgi:app
+/home/appuser/.local/bin/uwsgi --ini uwsgi.ini --socket 0.0.0.0:5000 --enable-threads --protocol=http -w wsgi:app
